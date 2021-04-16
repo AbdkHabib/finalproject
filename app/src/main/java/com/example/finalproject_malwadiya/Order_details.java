fbdf61bcfa8cf5538be14c6d532ebf6b0798c0db
AbdkHabib <80965727+AbdkHabib@users.noreply.github.com>
2021-04-16 06:45:45 +0300

    firebase

---
+++ app/src/main/java/com/example/finalproject_malwadiya/Order_details.java
@@ -0,0 +1,14 @@
+package com.example.finalproject_malwadiya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Order_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
